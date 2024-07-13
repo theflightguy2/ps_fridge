8250d83bf5cc26cd4ac54261911d5a952c46d7eb
theflightguy2 <amenbinehsan@gmail.com>
2024-07-13 23:34:41 +0100

    lattester working

---
+++ main.py
@@ -0,0 +1,19 @@
+#fridge
+import flet
+
+class FridgeObj():
+    def __init__(self, item, expiry, date_set, status):
+        self.item = item
+        self.expiry = expiry
+        self.date_set = date_set
+        self.status = status
+    
+    def expire(self):
+        self.item = 
+    sfsdfkjff
+    sdofjldsj
+    dsfjlks
+    sdjfjsdklfuiower
+    dlfksdjfh
+    dsflkjsdlkfs
+    dskfjsdlkfhkjsdgfuywerjhejkrh
